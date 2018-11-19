8790e0df950a82ccef74a05ae7c470d526bf2396
zabari <xyzddz@gmail.com>
2018-11-19 12:45:04 +0100

    cookie table added img added

--- JS/index.js
+++ JS/index.js
@@ -54,9 +54,35 @@ function openCar() {
 function ManufacturerCookie(man) {
     document.cookie = "name=" + man;
     $.getJSON('manufacturer', function (data) {
-        for(var i=0;i<data.length;i++)
-        alert(data[i].name);
+        /*for(var i=0;i<data.length;i++)
+        alert(data[i].name);*/
+
+        var table = $('<table class="CarTable"></table>');
+        table.append("<tr><th>Name</th><th>Consumption</th><th>Color</th><th>Manufacturer</th><th>Year</th><th>Available</th><th>Horsepower</th></tr>");
+
+        $.each(data, function (key, value) {
+            var row = $('<tr class="notFirstRow"></tr>');
+            var nameCell = $('<td >' + value.name + ' </td>');
+            var consumptionCell = $('<td>' + value.consumption + '</td>');
+            var colorCell = $('<td>' + value.color + '</td>');
+            var manufacturerCell = $('<td>' + value.manufacturer + '</td>');
+            var yearCell = $('<td>' + value.year + '</td>');
+            var availableCell = $('<td>' + value.available + '</td>');
+            var horsepowerCell = $('<td>' + value.horsepower + '</td>');
+
+            row.append(nameCell);
+            row.append(consumptionCell);
+            row.append(colorCell);
+            row.append(manufacturerCell);
+            row.append(yearCell);
+            row.append(availableCell);
+            row.append(horsepowerCell);
+            table.append(row);
+
+        });
+        $("#CookieTable").html(table);
     })
+
 }
 
 
@@ -68,8 +94,8 @@ function openManufacturer() {
         $.each(data, function (key, value) {
                 var row = $('<tr class="notFirstRow"></tr>');
                 var nameCell = $('<td onclick="ManufacturerCookie(' + "'" + value.name + "'" + ')">' + value.name + '</td>');
-                var countryCell = $('<td>' + value.country + '</td>');
-                var foundedCell = $('<td>' + value.founded + '</td>');
+                var countryCell = $('<td onclick="ManufacturerCookie(' + "'" + value.name + "'" + ')">' + value.country + '</td>');
+                var foundedCell = $('<td onclick="ManufacturerCookie(' + "'" + value.name + "'" + ')">' + value.founded + '</td>');
                 row.append(nameCell);
                 row.append(countryCell);
                 row.append(foundedCell);
